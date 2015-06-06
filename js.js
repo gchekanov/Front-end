0e5f0190ced6dd8e0b2335ec53e00f0f2a22670a
gchekanov <gchekanov@gmail.com>
2015-06-06 21:56:20 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,23 @@
+function mac()
+{
+	var x = document.getElementById("mac").innerHTML;
+	document.getElementById("buf").innerHTML = x;
+}
+
+function imac()
+{
+	var x = document.getElementById("imac").innerHTML;
+	document.getElementById("buf").innerHTML = x;
+}
+
+function ipod()
+{
+	var x = document.getElementById("ipod").innerHTML;
+	document.getElementById("buf").innerHTML = x;
+}
+
+function iphone()
+{
+	var x = document.getElementById("iphone").innerHTML;
+	document.getElementById("buf").innerHTML = x;
+}
